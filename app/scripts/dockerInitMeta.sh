035fc07d5c395337678e752f9779cda86ea6a114
raccoon-mh <wsx1341@gmail.com>
2024-10-18 16:12:24 +0000

    add scripts

--- app/scripts/dockerInitMeta.sh
+++ app/scripts/dockerInitMeta.sh
@@ -1,4 +1,4 @@
 mkdir -p ./container/meta
 cp -r ../meta/ ./container
 
-# docker run --rm -p 9000:8080 --name lol-team-maker-container lol-team-maker
+# docker run --rm -p 9000:8000 --name lol-team-maker-container lol-team-maker
