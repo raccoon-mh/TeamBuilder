f24bd31752406d74d8790a46b8920399c0069f7c
raccoon-mh <wsx1341@gmail.com>
2024-10-18 04:42:47 +0000

    add const value

---
+++ app/constvalue/constvalue.go
@@ -0,0 +1,13 @@
+package constvalue
+
+var (
+	TeminalRedStart     = "\033[31m"
+	TeminalEnd          = "\033[0m"
+	startBannerVersion  = "v0.0.1"
+	startBannerSubTitle = "Github = https://github.com/raccoon-mh/LoLTeamMaker , Author = raccoon-mh"
+	StartBanner         = "    __          __  ______                     __  ___      __            \n" +
+		"   / /   ____  / / /_  __/__  ____ _____ ___  /  |/  /___ _/ /_____  _____\n" +
+		"  / /   / __ \\/ /   / / / _ \\/ __ `/ __ `__ \\/ /|_/ / __ `/ //_/ _ \\/ ___/\n" +
+		" / /___/ /_/ / /___/ / /  __/ /_/ / / / / / / /  / / /_/ / ,< /  __/ /    \n" +
+		"/_____/\\____/_____/_/  \\___/\\__,_/_/ /_/ /_/_/  /_/\\__,_/_/|_|\\___/_/     " + TeminalRedStart + startBannerVersion + TeminalEnd + "\n" + startBannerSubTitle + "\n\n"
+)
