217c9905c594ed8c4f04b58c35128f604c1c9833
raccoon-mh <wsx1341@gmail.com>
2024-10-18 04:42:35 +0000

    add logger and limiter mw

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -2,18 +2,24 @@ package middleware
 
 import (
 	"log"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var LoggerConfig = middleware.RequestLoggerConfig{
-	LogStatus:   true,
-	LogURI:      true,
-	LogMethod:   true,
-	LogProtocol: true,
+var Logger = middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+	LogStatus: true, LogURI: true, LogMethod: true, LogProtocol: true, LogRemoteIP: true,
 	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-		log.Printf("[Logger] %v %v %v %v\n", v.Protocol, v.Method, v.Status, v.URI)
+		log.Printf("[Logger] %v - %v %v %v %v\n", v.RemoteIP, v.Protocol, v.Method, v.Status, v.URI)
 		return nil
 	},
-}
+})
+
+var Limitter = middleware.RateLimiter(middleware.NewRateLimiterMemoryStoreWithConfig(
+	middleware.RateLimiterMemoryStoreConfig{
+		Rate:      1,
+		Burst:     10,
+		ExpiresIn: 5 * time.Minute,
+	},
+))
